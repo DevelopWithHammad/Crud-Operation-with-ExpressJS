bc9aeacc39c116310847dcf4bf6c6bca5c6a6b62
Hammad <hammadurrehman700@gmail.com>
2023-08-28 16:11:04 +0500

    Crud operation

---
+++ studentsData.js
@@ -0,0 +1,54 @@
+const studentsData = [
+    {
+    "id": 1,
+    "first_name": "Gwennie",
+    "last_name": "Growcott",
+    "email": "ggrowcott0@marketwatch.com"
+  }, {
+    "id": 2,
+    "first_name": "Yule",
+    "last_name": "Daveran",
+    "email": "ydaveran1@joomla.org"
+  }, {
+    "id": 3,
+    "first_name": "Domini",
+    "last_name": "Stummeyer",
+    "email": "dstummeyer2@geocities.com"
+  }, {
+    "id": 4,
+    "first_name": "Cicely",
+    "last_name": "Megroff",
+    "email": "cmegroff3@cam.ac.uk"
+  }, {
+    "id": 5,
+    "first_name": "Dorthy",
+    "last_name": "Duckworth",
+    "email": "dduckworth4@cnet.com"
+  }, {
+    "id": 6,
+    "first_name": "Boote",
+    "last_name": "Novic",
+    "email": "bnovic5@feedburner.com"
+  }, {
+    "id": 7,
+    "first_name": "Sauncho",
+    "last_name": "Churchward",
+    "email": "schurchward6@shop-pro.jp"
+  }, {
+    "id": 8,
+    "first_name": "Celestine",
+    "last_name": "Macquire",
+    "email": "cmacquire7@msn.com"
+  }, {
+    "id": 9,
+    "first_name": "Pyotr",
+    "last_name": "Woodland",
+    "email": "pwoodland8@abc.net.au"
+  }, {
+    "id": 10,
+    "first_name": "Cliff",
+    "last_name": "Eilhart",
+    "email": "ceilhart9@networksolutions.com"
+  }]
+
+  export default studentsData
