f353b34a9ade6f4b7d78b5fba4e90e51e84a1a79
tabassumraza <tabssumshoaib@gmail.com>
2023-08-30 13:16:16 +0500

    till delete method

---
+++ index.js
@@ -0,0 +1,115 @@
+import express from "express";
+import fs from 'fs';
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware
+
+app.use(express.json());
+
+
+// reading toursData file
+
+const toursData = fs.readFileSync('./toursData.json', 'utf-8')
+const parsedToursData = JSON.parse(toursData)
+console.log(parsedToursData);
+
+
+// get method
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: "Success",
+        data: parsedToursData
+    })
+})
+
+
+
+app.get('/api/v1/tours/:tourId', (req, res) => {
+    // res.send(req.params.tourId)
+    console.log("tourId===>>>>>", req.params.tourId);
+    console.log("typeof===>>>>>", typeof req.params.tourId);
+    console.log("===>>>>>length", parsedToursData?.data.length);
+
+    if ((req.params.tourId * 1) > parsedToursData?.data.length) {
+        res.status(404).send("Data not available")
+    } else {
+        res.status(200).send(req.params.tourId)
+    }
+
+
+    const singleTour = parsedToursData?.data?.find(tour => tour.id == req.params.tourId)
+
+    console.log("===>>>Single tour", singleTour);
+
+    res.status(200).json({
+        status: "Success",
+        data: singleTour
+    })
+})
+
+
+
+app.post("/api/v1/tours", (req, res) => {
+    console.log("====>>> Available tours", parsedToursData?.data.length);
+
+    // ||
+    if (!req.body.destinationName || !req.body.country || !req.body.tourCapacity || !req.body.departure || !req.body.arrival || !req.body.price || !req.body.transport) {
+        res.status(400).json({
+            status: "Rejected",
+            data: "Missing Fields"
+        })
+    }
+
+    const dataToWriteInDB = {
+        id: parsedToursData.data.length + 1,
+        ...req.body
+    }
+    console.log(dataToWriteInDB);
+
+    parsedToursData.data.push(dataToWriteInDB)
+
+
+
+    fs.writeFile("./toursData.json", JSON.stringify(parsedToursData), () => {
+        res.status(200).json({
+            status: "Success",
+            data: "Data added succcessfully!"
+        })
+    })
+})
+
+
+app.delete("/api/v1/tours/:tourId", (req, res) => {
+    console.log(req.param.tourId);
+    const filteredData = parsedToursData.data.filter(tour => tour.id != (req.params.tourId * 1))
+
+    parsedToursData.data = filteredData
+
+    fs.writeFile("./toursData.json", JSON.stringify(parsedToursData), () => {
+        res.status(200).json({
+            status: "Success",
+            data: "succcessfully deleted!"
+        })
+    })
+})
+
+
+
+app.put("/api/v1/tours/:tourId", (req, res) => {
+    console.log("id====>>>>", req.params.tourId);
+
+    let indexNumber;
+
+   
+
+
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`I am listening to the server at port number ${PORT}`);
+})
